b9441906d6dca280c46bb525b8970fe43f401bb1
ZeroKun265 <davidegiambirtone265@gmail.com>
2025-06-21 19:05:34 +0200

    triggering hyprlock and using the logout script that tries to fixx sddm's shenanigans

--- waybar/scripts/powermenu.sh
+++ waybar/scripts/powermenu.sh
@@ -7,7 +7,7 @@ chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu")
 case "$chosen" in
   Shutdown) systemctl poweroff ;;
   Reboot) systemctl reboot ;;
-  Suspend) systemctl suspend ;;
-  Hibernate) systemctl hibernate ;;
-  Logout) hyprctl dispatch exit ;;
+  Suspend) hyprlock & systemctl suspend ;;
+  Hibernate) hyprlock & systemctl hibernate ;;
+  Logout) logout_hyprland ;;
 esac
